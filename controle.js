16bfff59074e7bf9b5947257f25a9d133d1a9eed
renata0408 <renata.cristovam@escola.pr.gov.br>
2025-08-20 12:14:31 -0300

    Add files via upload

---
+++ controle.js
@@ -0,0 +1,18 @@
+function alterarNome(){
+    var n = document.getElementById("nome")
+n.innerHTML = `renata`
+}
+
+
+
+
+
+function on(){
+     document.getElementById("lamp").src = "on.jpg"
+}
+function off(){
+    document.getElementById("lamp").src = "off.jpg"
+}
+function broken(){
+    document.getElementById("lamp").src = "broken.jpg"
+}
